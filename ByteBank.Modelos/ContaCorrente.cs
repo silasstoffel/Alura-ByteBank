30cacf670aec68c42eb0539b7d9c8fe4c6b97b87
Silas Stoffel <silasstofel@gmail.com>
2020-10-14 21:28:35 -0300

    Documentação da conta corrente

--- ByteBank.Modelos/ContaCorrente.cs
+++ ByteBank.Modelos/ContaCorrente.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ByteBank.Modelos
 {
     /// <summary>
-    /// Define uma Conta corrente do banco ByteBank
+    /// Define uma Conta Corrente do banco ByteBank.
     /// </summary>
     public class ContaCorrente
     {
@@ -42,10 +42,10 @@ namespace ByteBank.Modelos
         }
 
         /// <summary>
-        /// Cria uma conta corrente com os argumentos
+        /// Cria uma instância de ContaCorrente com os argumentos utilizados.
         /// </summary>
-        /// <param name="agencia">Representa agência (<see cref="Agencia"/>), o valor deve ser maior que zero</param>
-        /// <param name="numero">Representa o número de uma conta corrente (<see cref="Numero"/>). O valor deve ser maior que zero</param>
+        /// <param name="agencia"> Representa o valor da propriedade <see cref="Agencia"/> e deve possuir um valor maior que zero. </param>
+        /// <param name="numero"> Representa o valor da propriedade <see cref="Numero"/> e deve possuir um valor maior que zero. </param>
         public ContaCorrente(int agencia, int numero)
         {
             if (numero <= 0)
@@ -65,6 +65,12 @@ namespace ByteBank.Modelos
             TaxaOperacao = 30 / TotalDeContasCriadas;
         }
 
+        /// <summary>
+        /// Realiza o saque e atualiza o valor da propriedade <see cref="Saldo"/>.
+        /// </summary>
+        /// <exception cref="ArgumentException"> Exceção lançada quando um valor negativo é utilizado no argumento <paramref name="valor"/>. </exception>
+        /// <exception cref="SaldoInsuficienteException"> Exceção lançada quando o valor de <paramref name="valor"/> é maior que o valor da propriedade <see cref="Saldo"/>. </exception>
+        /// <param name="valor"> Representa o valor do saque, deve ser maior que 0 e menor que o <see cref="Saldo"/>. </param>
         public void Sacar(double valor)
         {
             if (valor < 0)
